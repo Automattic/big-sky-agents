d393ea1e63b5ce4f3eeb25c3858b5db12860b7d8
Daniel Walmsley <dan.walmsley@a8c.com>
2024-08-02 13:07:31 -0700

    Remove useTools and fix module setup

--- src/components/toolkits-provider/use-toolkits.js
+++ src/components/toolkits-provider/use-toolkits.js
@@ -7,8 +7,8 @@ import { useDispatch, useSelect } from '@wordpress/data';
 /**
  * Internal dependencies
  */
-import defaultToolkits from '../../ai/toolkits/default-toolkits';
-import useAgents from '../agents-provider/use-agents';
+import defaultToolkits from '../../ai/toolkits/default-toolkits.js';
+import useAgents from '../agents-provider/use-agents.js';
 
 /**
  * Internal dependencies
@@ -120,7 +120,7 @@ function resolveAgentTools( agent, context, toolkitTools ) {
 export default function useToolkits() {
 	const toolkitsStore = useContext( Context );
 	const { activeAgent } = useAgents();
-	const { registerToolkit, setCallbacks, setContext, setTools } =
+	const { registerToolkit, setCallbacks, setContext } =
 		useDispatch( toolkitsStore );
 	const { call, agentSay, userSay } = useChat();
 	const {
@@ -253,7 +253,6 @@ export default function useToolkits() {
 		registerToolkit,
 		setCallbacks,
 		setContext,
-		setTools,
 		registerDefaultToolkits,
 	};
 }
