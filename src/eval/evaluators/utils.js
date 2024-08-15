8f45f1cd217937da43909b2b2976dad6b01ae7f6
kthai15 <kavin.keratikasama@automattic.com>
2024-08-15 10:58:48 -0700

    update check for array equality

--- src/eval/evaluators/utils.js
+++ src/eval/evaluators/utils.js
@@ -10,6 +10,17 @@ export const deepEqual = ( obj1, obj2 ) => {
 	) {
 		return false;
 	}
+	if ( Array.isArray( obj1 ) && Array.isArray( obj2 ) ) {
+		if ( obj1.length !== obj2.length ) {
+			return false;
+		}
+		// Sort both arrays and compare them
+		const sortedObj1 = [ ...obj1 ].sort();
+		const sortedObj2 = [ ...obj2 ].sort();
+		return sortedObj1.every( ( item, index ) =>
+			deepEqual( item, sortedObj2[ index ] )
+		);
+	}
 	const keys1 = Object.keys( obj1 );
 	const keys2 = Object.keys( obj2 );
 	if ( keys1.length !== keys2.length ) {
