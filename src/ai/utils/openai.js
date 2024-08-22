c6e6f30413875baff9dc39a0a68d59b592fafe07
kthai15 <kavin.keratikasama@automattic.com>
2024-08-22 15:42:35 -0700

    opt-in to structured output

--- src/ai/utils/openai.js
+++ src/ai/utils/openai.js
@@ -1,17 +1,25 @@
-export const toOpenAITool = ( tool ) => ( {
-	type: 'function',
-	function: {
-		name: tool.name,
-		description: tool.description,
-		// default to a single string parameter called "value"
-		parameters: tool.parameters ?? {
-			type: 'object',
-			properties: {
-				value: {
-					type: 'string',
-				},
+export const toOpenAITool = ( tool ) => {
+	// default to a single string parameter called "value"
+	const parameters = tool.parameters ?? {
+		type: 'object',
+		properties: {
+			value: {
+				type: 'string',
 			},
-			required: [ 'value' ],
 		},
-	},
-} );
+		required: [ 'value' ],
+		additionalProperties: ! tool.strict,
+	};
+
+	// enable structured output if strict is true
+	parameters.additionalProperties = ! tool.strict;
+	return {
+		type: 'function',
+		function: {
+			name: tool.name,
+			description: tool.description,
+			parameters,
+		},
+		strict: !! tool.strict,
+	};
+};
